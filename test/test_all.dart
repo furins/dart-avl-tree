a3108cb15bda992c8a0950e567f12a364d3f77b7
Gubaer <karl.guggisberg@guggis.ch>
2013-05-05 21:20:14 +0200

    added test_all.dart

---
+++ test/test_all.dart
@@ -0,0 +1,9 @@
+library test_all;
+
+import "test_avl_tree.dart" as test_avl_tree;
+import "test_avl_tree_with_line_segments.dart" as test_avl_tree_with_line_segments;
+
+main() {
+  test_avl_tree.main();
+  test_avl_tree_with_line_segments.main();
+}
